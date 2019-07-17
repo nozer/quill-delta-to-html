93d7f94ad2ad8091da94b8cb662c57f6b55ddecc
kfir.z <kfir.z@walkme.com>
2019-07-17 14:00:53 +0300

    export

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 
 // main entry file for node
 export { QuillDeltaToHtmlConverter } from './QuillDeltaToHtmlConverter';
+export { OpToHtmlConverter } from './OpToHtmlConverter';
 export { InlineGroup, VideoItem, BlockGroup, ListGroup, ListItem, BlotBlock } from "./grouper/group-types";
 export { DeltaInsertOp } from "./DeltaInsertOp";
 export { InsertDataQuill, InsertDataCustom } from "./InsertData";
